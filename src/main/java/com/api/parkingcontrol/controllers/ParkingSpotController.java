89448d22ccd22252ef60f8dbaf59ce27fdbac2fc
Victor Carpes <victorcarpes@outlook.com>
2023-09-27 09:33:08 +0100

    First commit

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,4 @@
+package com.api.parkingcontrol.controllers;
+
+public class ParkingSpotController {
+}
---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotRepository(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+}
